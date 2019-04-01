683b5ff10ad63b0ece0b93e3a742bd40025c9de3
tdaleyy <47867652+tdaleyy@users.noreply.github.com>
2019-03-31 23:27:07 -0300

    Updated the excel reader class to create and return transcript objects
    Updated the excel reader class to create and return transcript objects

--- ExcelReader.java
+++ ExcelReader.java
@@ -1,79 +1,146 @@
 package team_project;
 
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class ExcelReader{
 	
-	private static ArrayList<Transcript> TranscriptList = new ArrayList<Transcript>();
-	static FileInputStream is;
-	static XSSFWorkbook wb;
-	static XSSFSheet sheet;
-	XSSFRow row;
-	XSSFCell cell;
+	private static FileInputStream fis;
+	private static XSSFWorkbook workbook;
+	private static XSSFSheet spreadsheet;
+	private static XSSFRow row;
 	
-	
-	protected static void transcriptCollector() {
+	protected static void openExcel(String filepath) {
 		
 		try {
-			is = new FileInputStream(ConfigGUI.getTranscriptFolderPath() + "exScript.xlsx");
-			wb = new XSSFWorkbook(is);
-			sheet = wb.getSheetAt(0);
-		}catch(FileNotFoundException e) {System.out.println("Error: Could not find specified file.");} 
-		catch (IOException e) {System.out.println("Error: Could not find workbook.");}
-		
-		
-		for(int i = 0; i <= sheet.getLastRowNum(); i++) {
 			
-			try{
-				int cell = 0;
-				
-				String courseCode = sheet.getRow(i).getCell(cell).getStringCellValue();
-				cell++;
-				String sectionCode = sheet.getRow(i).getCell(cell).getStringCellValue();
-				cell++;
-				String title = sheet.getRow(i).getCell(cell).getStringCellValue();
-				cell++;
-				String letterGrade = sheet.getRow(i).getCell(cell).getStringCellValue();
-				cell++;
-				double creditHour = sheet.getRow(i).getCell(cell).getNumericCellValue();
-				cell++;
-				String term = sheet.getRow(i).getCell(cell).getStringCellValue();
-				
-				Transcript transcript = new Transcript(courseCode, sectionCode, title, letterGrade, creditHour, term);
-				
-				TranscriptList.add(transcript);
-				
-				System.out.println("line done");
-				
-			}catch(NullPointerException e){System.out.println("Error: End of file."); break;}
-		}
+			fis = new FileInputStream(filepath);
+			workbook = new XSSFWorkbook(fis);
+			spreadsheet = workbook.getSheetAt(0);
+			System.out.println("Success: Excel Document Opened.\n");
 			
-		
+		}catch (Exception e){
+			
+			System.out.println("Error: Excel Document not found.");}
+	}
+	
+	protected static void printExcelFile() {
+		try{
+			int currentCell;
+			Iterator < Row > rowIterator = spreadsheet.iterator();
+			
+			try{row = (XSSFRow) rowIterator.next();
+			}catch(NullPointerException e){System.out.println("Error: Could not find next row.");}
+			
+			System.out.println("Excel File Printed Contents:\n-----------------------------------"
+								+ "--------------------------------------------------");
+			
+			while(rowIterator.hasNext()){
+				currentCell = 0;
+				while (currentCell < 6){
+					
+					if(row.getCell(currentCell)==null || row.getCell(currentCell).getCellType()==CellType.BLANK)
+						currentCell++;
+					else {
 				
+						try{
+							System.out.print(row.getCell(currentCell).toString() + "\t");							
+						
+						}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+						currentCell++;
+					}
+				
+				}//end of inner while loop
+				System.out.println();
+				try{
+					row = (XSSFRow) rowIterator.next();
+				}catch(NullPointerException e){System.out.println("Error: Could not find next row.");break;}
+				
+			}//end of outer while loop
+		}catch (Exception e){System.out.println("Error: Could not read excel file.");}
+		System.out.println("-------------------------------------------------------------------------------------\n");
+	}
+	
+	protected static Transcript readExcelFile(/*ArrayList<course> tScript*/) {
+		
+		Transcript t = new Transcript();
+		String courseCode = null, sectionCode = null, title = null, letterGrade = null, creditHour = null, term = null;
+		int currentCell;
+		
+		try{Iterator < Row > rowIterator = spreadsheet.iterator();
+		
+			try{row = (XSSFRow) rowIterator.next();
+			}catch(NullPointerException e){System.out.println("Error: Could not find next row.");}
+			
+			while(rowIterator.hasNext()){
+				currentCell = 0;
+				while (currentCell < 6){
+				
+						if(currentCell==0) {
+							try{courseCode = row.getCell(currentCell).toString();
+							}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+							currentCell++;
+						}
+						
+						if(currentCell==1) {
+							try{sectionCode = row.getCell(currentCell).toString();
+							}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+							currentCell++;
+						}
+						
+						if(currentCell==2) {
+							try{title = row.getCell(currentCell).toString();
+							}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+							currentCell++;
+						}
+						
+						if(currentCell==3) {
+							try{letterGrade = row.getCell(currentCell).toString();
+							}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+							currentCell++;
+						}
+						
+						if(currentCell==4) {
+							try{creditHour = row.getCell(currentCell).toString();
+							}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+							currentCell++;
+						}
+						
+						if(currentCell==5) {
+							try{term = row.getCell(currentCell).toString();
+							}catch(NullPointerException e){System.out.println("Error: Could not read cell data.");break;}
+							currentCell++;
+						}
+						
+				}//end of inner while loop
+				
+				t.add(new Course(courseCode, sectionCode, title, letterGrade, creditHour, term));
+				try{row = (XSSFRow) rowIterator.next();
+				}catch(NullPointerException e){System.out.println("Error: Could not find next row.");break;}
+				
+			}//end of outer while loop
+			
+			System.out.println(t.toString());
+			
+		}catch (Exception e){System.out.println("Error: Could not read excel file.");}
+		return t;
 	}
 	
 	protected static void closeExcel() {
 		
 		try{
 			
-			wb.close();
-			is.close();
+			fis.close();
 			
 		}catch(Exception e){
 			
 			System.out.println("Error: Input stream could not be closed. ");
 		}
 	}
-	
-	
-	
 }
