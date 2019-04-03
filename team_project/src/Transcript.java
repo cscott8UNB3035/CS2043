a439a0d85de405e5ed1da08564fcd551f60eb259
Coby Pugh <coby@cobypugh.com>
2019-04-03 15:36:22 -0300

    Fixed package structure

--- team_project/src/Transcript.java
+++ team_project/src/Transcript.java
@@ -1,68 +1,29 @@
 package team_project;
 
+import java.util.ArrayList;
+
 public class Transcript {
 	
-	private String courseCode;
-	private String sectionCode;
-	private String title;
-	private String letterGrade;
-	private double creditHour;
-	private String term;
+	private ArrayList<Course> tScript;
 	
-	protected Transcript(String courseCode, String sectionCode, String title, String letterGrade, double creditHour, String term) {
-		this.courseCode = courseCode;
-		this.sectionCode = sectionCode;
-		this.title = title;
-		this.letterGrade = letterGrade;
-		this.creditHour = creditHour;
-		this.term = term;
+	protected Transcript() {
+		super();
+		this.tScript = new ArrayList<Course>();
+	
+	protected void add(Course c) {
+		this.tScript.add(c);
 	}
 
-	protected String getCourseCode() {
-		return courseCode;
+	public String toString() {
+		String output = "Transcript Object:\n-------------------------------------"
+						+ "------------------------------------------------\n";
+		int max = this.tScript.size();
+		for(int i = 0; i<max; i++) {
+			output+=this.tScript.get(i).toString();
+		}
+		output = output + "-------------------------------------"
+						+ "------------------------------------------------\n";
+		return output;
 	}
-
-	protected void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
-	}
-
-	protected String getSectionCode() {
-		return sectionCode;
-	}
-
-	protected void setSectionCode(String sectionCode) {
-		this.sectionCode = sectionCode;
-	}
-
-	protected String getTitle() {
-		return title;
-	}
-
-	protected void setTitle(String title) {
-		this.title = title;
-	}
-
-	protected String getLetterGrade() {
-		return letterGrade;
-	}
-
-	protected void setLetterGrade(String letterGrade) {
-		this.letterGrade = letterGrade;
-	}
-
-	protected double getCreditHour() {
-		return creditHour;
-	}
-
-	protected void setCreditHour(double creditHour) {
-		this.creditHour = creditHour;
-	}
-
-	protected String getTerm() {
-		return term;
-	}
-
-	protected void setTerm(String term) {
-		this.term = term;
-	}
+	
 }
