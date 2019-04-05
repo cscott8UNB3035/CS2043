8f8e6473f4c28b6e9e186c60df02513ee40bce17
tdaleyy <47867652+tdaleyy@users.noreply.github.com>
2019-04-04 22:16:46 -0300

    added text reader class and modified driver
    updated

--- Driver.java
+++ Driver.java
@@ -1,12 +1,13 @@
-package team_project;
-
-//import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Driver {
 
 	//private static Scanner input = new Scanner(System.in);
-	private static String filepath1="C:\\Users\\tdale\\Documents\\School\\2018-2019\\CS2043 - Software Engineering\\Project\\D3\\exScript1.xlsx";
-	private static String filepath2="C:\\Users\\tdale\\Documents\\School\\2018-2019\\CS2043 - Software Engineering\\Project\\D3\\exScript2.xlsx";
+	//C:\Users\tdale\Documents\School\2018-2019\CS2043 - Software Engineering\Project\D4\cohort_1\cohort_1\2015EE_1.txt
+	private static String filepath1="C:\\Users\\tdale\\Documents\\School\\2018-2019\\CS2043 - Software Engineering\\Project\\D4\\cohort_1\\cohort_1\\2015EE_1.txt";
+	private static String filepath2="C:\\Users\\tdale\\Documents\\School\\2018-2019\\CS2043 - Software Engineering\\Project\\D4\\cohort_1\\cohort_1\\2015EE_2.txt";
 	
 	public static void main(String[] args) {
 		
@@ -17,26 +18,44 @@ public class Driver {
 		String creditHour = "5.0";
 		String term = "2011/WI";
 		
+		File file1 = new File(filepath1);
+		File file2 = new File(filepath2);
+		
 		Course c = new Course(courseCode, sectionCode, title, letterGrade, creditHour, term);
 		System.out.println("test: Create and print course object:\n" + c.toString());
 		
-		//C:\\Users\\tdale\\Documents\\School\\2018-2019\\CS2043 - Software Engineering\\Project\\D3\\exScript1.xlsx
-		/*System.out.println("Enter file path:");
-		filepath = input.nextLine();
-		*/
+		try {
+			TextReader.openFile(file1);
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: file not found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error: file not found");
+			e.printStackTrace();
+		}
 		
-		TranscriptHandler th = new TranscriptHandler();
+		try {
+		TextReader.readTextFile();
+		} catch(NullPointerException e) {
+			System.out.println("\n"+e+"\n");
+		}
+		/*TextReader.closeFile();*/
 		
-		ExcelReader.openExcel(filepath1);
-		ExcelReader.printExcelFile();
-		th.add(ExcelReader.readExcelFile());
-		ExcelReader.closeExcel();
-		
-		ExcelReader.openExcel(filepath2);
-		ExcelReader.printExcelFile();
-		th.add(ExcelReader.readExcelFile());
-		ExcelReader.closeExcel();
-		
-		System.out.println(th.toString());
+		try {
+			TextReader.openFile(file2);
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: file not found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error: file not found");
+			e.printStackTrace();
+		}
+
+		try {
+			TextReader.readTextFile();
+		} catch(NullPointerException e) {
+			System.out.println("\n"+e+"\n");
+		}
+		/*TextReader.closeFile();*/
 	}
 }
