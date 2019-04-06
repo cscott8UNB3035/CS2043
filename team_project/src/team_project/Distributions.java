2cd292ff4502f897d1fd726bfb218277205757a3
jburges2 <jburges2@unb.ca>
2019-04-06 17:12:15 -0300

    Further revisions of the class

--- team_project/src/team_project/Distributions.java
+++ team_project/src/team_project/Distributions.java
@@ -25,53 +25,90 @@ public class Distributions {
 	
 	protected Distributions getAreaDistributions(TranscriptHandler tHandler, Area area) {
 		int num = 0;
-		for(int i = 0; i <= list.getSize(); i++) {
-			for(int j = 0; j <= list.getTranscript(j).size(); j++) {
-				if(list.getTranscript(j))
-			}
-		}
-	}
-	
-	protected Distributions getRawDistributions(ArrayList<Course>, tScript) {
-		int num = 0;
-		for(int i = 0; i <= tScript.size(); i++) {
-			if(courseCode.get(i).equals(tScript.get(i).getCourseCode())) {
-				if(tScript.get(i).getLetterGrade() == "F" || tScript.get(i).getLetterGrade() == "D") {
-					num = 2;
-				}
-				else if(tScript.get(i).getLetterGrade() == "C" || tScript.get(i).getLetterGrade() == "C+") {
-					num = 3;
-				}
-				else if(tScript.get(i).getLetterGrade() == "B-" || tScript.get(i).getLetterGrade() == "B" || tScript.get(i).getLetterGrade() == "B+") {
-					num = 4;
-				}
-				else if(tScript.get(i).getLetterGrade() == "A-" || tScript.get(i).getLetterGrade() == "A" || tScript.get(i).getLetterGrade() == "A+") {
-					num = 5;
-				}
-				else {
-					num = 1;
-				}
-				courseCode.add(tScript.get(i).getCourseCode());
-				switch(num) {
-					case 1: other = other + 1;
-							others.add(other);
-					case 2: fail = fail + 1;
-							fails.add(fail);
-							break;
-					case 3: marginal = marginal + 1;
-							marginals.add(marginal);
-							break;
-					case 4: meet = meet + 1;
-							meets.add(meet);
-							break;
-					case 5: exceed = exceed + 1;
-							exceeds.add(exceed);
-							break; 
+		for(int i = 0; i <= tHandler.getList.getSize(); i++) {
+			for(int j = 0; j <= tHandler.getList.getTranscript(j).size(); j++) {
+				if(tHandler.getList.getTranscript(j).get(j).equals(courseCode.get(i))) {
+					if(tScript.get(i).getLetterGrade() == "F" || tScript.get(i).getLetterGrade() == "D") {
+						num = 2;
+					}
+					else if(tScript.get(i).getLetterGrade() == "C" || tScript.get(i).getLetterGrade() == "C+") {
+						num = 3;
+					}
+					else if(tScript.get(i).getLetterGrade() == "B-" || tScript.get(i).getLetterGrade() == "B" || tScript.get(i).getLetterGrade() == "B+") {
+						num = 4;
+					}
+					else if(tScript.get(i).getLetterGrade() == "A-" || tScript.get(i).getLetterGrade() == "A" || tScript.get(i).getLetterGrade() == "A+") {
+						num = 5;
+					}
+					else {
+						num = 1;
+					}
+					courseCode.add(tScript.get(i).getCourseCode());
+					switch(num) {
+						case 1: other = other + 1;
+								others.add(other);
+						case 2: fail = fail + 1;
+								fails.add(fail);
+								break;
+						case 3: marginal = marginal + 1;
+								marginals.add(marginal);
+								break;
+						case 4: meet = meet + 1;
+								meets.add(meet);
+								break;
+						case 5: exceed = exceed + 1;
+								exceeds.add(exceed);
+								break; 
+					}
 				}
 			}
 		}
 		Distributions distribution = new Distributions(courseCode, others, fails, marginals, meets, exceeds);
 		return distribution;
+	}
+	
+	protected Distributions getRawDistributions(TranscriptHandler tHandler>, tScript) {
+		int num = 0;
+		for(int i = 0; i <= tHandler.getList.getSize(); i++) {
+			for(int j = 0; j <= tHandler.getList.getTranscript(j).size(); j++) {
+				if(tHandler.getList.getTranscript(j).get(j).equals(courseCode.get(i))) {
+					if(tScript.get(i).getLetterGrade() == "F" || tScript.get(i).getLetterGrade() == "D") {
+						num = 2;
+					}
+					else if(tScript.get(i).getLetterGrade() == "C" || tScript.get(i).getLetterGrade() == "C+") {
+						num = 3;
+					}
+					else if(tScript.get(i).getLetterGrade() == "B-" || tScript.get(i).getLetterGrade() == "B" || tScript.get(i).getLetterGrade() == "B+") {
+						num = 4;
+					}
+					else if(tScript.get(i).getLetterGrade() == "A-" || tScript.get(i).getLetterGrade() == "A" || tScript.get(i).getLetterGrade() == "A+") {
+						num = 5;
+					}
+					else {
+						num = 1;
+					}
+					courseCode.add(tScript.get(i).getCourseCode());
+					switch(num) {
+						case 1: other = other + 1;
+								others.add(other);
+						case 2: fail = fail + 1;
+								fails.add(fail);
+								break;
+						case 3: marginal = marginal + 1;
+								marginals.add(marginal);
+								break;
+						case 4: meet = meet + 1;
+								meets.add(meet);
+								break;
+						case 5: exceed = exceed + 1;
+								exceeds.add(exceed);
+								break; 
+					}
+				}
+			}
+		}
+		Distributions distribution = new Distributions(courseCode, others, fails, marginals, meets, exceeds);
+		return distribution;s
 	}
 	
 	
